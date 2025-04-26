6c982a4d2658d852335b29778541cb3daf1062a9
JEYASRI M <jesusjeba2510@gmail.com>
2025-04-26 20:35:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the pre-trained model (update to use the correct model file)
-model = joblib.load('C:/Users/jesus/OneDrive/Desktop/inventory demand forecasting/model.pkl')  # Use your existing model file
+model = joblib.load('model.pkl')  # Use your existing model file
 
 # Load dataset (if needed for making predictions or validating inputs)
 df = pd.read_csv('retail_store_inventory.csv')
